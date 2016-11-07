7caf6ea05de5452c79e43cca7b57beb0d05ace2a
int3n5ii0n <m4lc0d3sec@gmail.com>
2016-11-07 01:10:53 -0500

    Add files via upload

---
+++ Sequence.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sequence;
+
+/**
+ *
+ * @author ghx5t
+ */
+public class Sequence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Sequence Activity
+        System.out.println("Welcome to Sequence");
+        
+    }
+    
+}
